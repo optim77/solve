fff5605d7360abfd3e12d392b5866959a231c69a
Kuba <plajerowy@gmail.com>
2025-09-03 19:25:05 +0200

    post payment impl 2

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -60,6 +60,8 @@ export async function POST(req: Request) {
             cancel_url: `${origin}/protected/chat`,
         });
 
+
+
         return NextResponse.json({ url: session.url });
     } catch (err: any) {
         return NextResponse.json({ error: err.message }, { status: 500 });
--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
+import { createClient } from "@supabase/supabase-js";
 
-export const runtime = "nodejs"; // 🔑
 
 export async function POST(req: Request) {
-    const sig = req.headers.get("stripe-signature");
     const body = await req.text();
-    console.log("HOOK");
+    const sig = req.headers.get("stripe-signature");
+
     let event;
+
     try {
         event = stripe.webhooks.constructEvent(
             body,
@@ -15,16 +16,85 @@ export async function POST(req: Request) {
             process.env.STRIPE_WEBHOOK_SECRET!
         );
     } catch (err: any) {
-        console.error("❌ Webhook signature verification failed:", err.message);
+        console.error("Webhook signature verification failed.", err.message);
         return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
     }
 
-    console.log("✅ Webhook event:", event.type);
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!);
+    try {
+        switch (event.type) {
+            case "checkout.session.completed": {
+                const session = event.data.object as any;
 
-    if (event.type === "checkout.session.completed") {
-        const session = event.data.object as any;
-        console.log("🎯 Session completed for:", session.id);
+                if (session.mode === "subscription") {
+
+                    await supabase
+                        .from("subscriptions")
+                        .upsert({
+                            user_id: session.client_reference_id,
+                            plan_id: session.metadata.productId,
+                            months: 1,
+                            active: true
+                        }, {
+                            onConflict: "user_id",
+                            ignoreDuplicates: false
+                        });
+
+                    const {  error } = await supabase.rpc('insert_purchase_subscription', {
+                        uid: session.client_reference_id,
+                        plans_id: session.metadata.productId,
+                        checkout_session_id: session.checkout_session_id
+                    });
+                    if (error){
+                        throw error;
+                    }
+                } else {
+
+                    const { data: userCredits } = await supabase
+                        .from("profiles")
+                        .select("credits")
+                        .eq("user_id", session.client_reference_id)
+                        .single();
+
+                    console.log(userCredits);
+                    if (userCredits) {
+
+                        await supabase.rpc("increment_credits", {
+                            user_id: session.client_reference_id,
+                            amount: userCredits.credits,
+                            checkout_session_id: session.checkout_session_id
+                        });
+                    }
+
+                    const {  error } = await supabase
+                        .from("purchase")
+                        .insert({
+                            "user_id": session.client_reference_id,
+                            "credits_id": session.metadata.productId,
+                            "checkout_session_id": session.checkout_session_id
+                        })
+                        .select()
+                        .single();
+                        // .rpc('insert_purchase_credits', {
+                        // uid: session.client_reference_id,
+                        // credits_id: session.metadata.productId,
+                        // checkout_session_id: session.checkout_session_id
+                    if (error){
+                        throw error;
+                    }
+                }
+                break;
+            }
+
+            case "invoice.payment_failed": {
+                console.log("Payment failed", event.data.object);
+                break;
+            }
+        }
+
+        return NextResponse.json({ received: true });
+    } catch (err: any) {
+        console.error("Webhook error:", err.message);
+        return new NextResponse("Webhook error", { status: 500 });
     }
-
-    return NextResponse.json({ received: true });
 }
